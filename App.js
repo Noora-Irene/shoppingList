a0e611f495a6a53b0c64f9d65b9cd5821150bb88
Noora-Irene <nooraetel@gmail.com>
2020-03-26 08:58:53 +0200

    using sqlite database

--- App.js
+++ App.js
@@ -1,51 +1,78 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, FlatList } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { StatusBar, StyleSheet, View, Text, TextInput, Button, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('shoppingdb.db');
 
 export default function App() {
 
-  const [product, setInput] = useState('');
-  const [productList, setProduct] = useState([]);
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [productList, setOnList] = useState([]);
 
-  const userInput = text => {   
-    setInput(text);
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shopping (id integer primary key not null, amount int, product text);');
+    }, null, updateList);    
+  }, []);
+
+  const input1 = text => {   
+    setProduct(text);
   };
-  const addOnList = () => {
-    setProduct(currentProducts => [...currentProducts, { key: Math.random().toString(), value: product }
-    ]);
-    setInput('');
-  }
-  const clearList = productList => {
-    setProduct(currentProducts => {
-      return ('')
+  const input2 = text => {   
+    setAmount(text);
+  };
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shopping;', [], (_, { rows }) =>
+        setOnList(rows._array)
+      ); 
     });
   }
+  const addOnList = () => {
+    db.transaction(tx => {
+      tx.executeSql('insert into shopping (amount, product) values (?, ?);', [parseInt(amount), product]);
+    }, null, updateList
+    )
+    setProduct('');
+    setAmount('');
+  }
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => {
+        tx.executeSql('delete from shopping where id = ?;', [id]);
+      }, null, updateList
+    )    
+  }
+  // const deleteall = (id) => {  }
 
   return (
     <View style={styles.wholeScreen}>
+      <StatusBar hidden={true} />
         <View style={styles.inputArea}>
-          <TextInput style={styles.input} onChangeText={userInput} value={product}
+          <TextInput style={styles.input} placeholder= 'Product' onChangeText={input1} value={product}
+          />
+          <TextInput style={styles.input} placeholder= 'Amount' onChangeText={input2} value={amount}
           />
         </View>
-
         <View style={styles.buttonArea}>
-
           <View style={styles.button}>
             <Button title="ADD" onPress={addOnList} />
           </View>
-
-          <View style={styles.button}>
-            <Button title="CLEAR" color= 'red' onPress={clearList} />
-          </View>
+         {/*} <View style={styles.button}>
+            <Button title="CLEAR" color= 'red' onPress={deleteall} />
+          </View> */}
         </View>
-        <View style={styles.listArea}>
-          <Text style={styles.titleText}>Shopping list</Text>
+          <View style={styles.title}><Text style={styles.titleText}>Shopping list</Text></View>
             <FlatList
+              keyExtractor={item => item.id.toString()}
+              renderItem={ ({ item }) =>
+              <View style={styles.listArea}>
+                <Text style={styles.listText}> {item.product}, {item.amount} </Text> 
+                <Text style={styles.deleteText} onPress={() => deleteItem(item.id) }> Bought </Text>
+              </View> }
               data={productList}
-              renderItem={itemData => (
-                <Text style={styles.listText}> {itemData.item.value} </Text>
-          )}
-        />
-        </View>
+            />
     </View>
   );
 }
@@ -53,20 +80,20 @@ const styles = StyleSheet.create({
   wholeScreen: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   inputArea: {
-    flex: 2,
+    flex: 1,
     justifyContent: 'flex-end'
   },
   buttonArea: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    marginTop: 20,
+    justifyContent: 'flex-end'
   },
-  listArea: {
-    flex: 5,
+  title: {
+    flex: 1,
     justifyContent: 'flex-start'
   },
   titleText: {
@@ -74,9 +101,19 @@ const styles = StyleSheet.create({
     color: 'purple',
     fontWeight: 'bold'
   },
+  listArea: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start'
+  },
   listText: {
     fontSize: 16,
     color: 'purple'
+  },
+  deleteText: {
+    fontSize: 16,
+    color: 'blue'
   },
   input: {
     width: 200,
